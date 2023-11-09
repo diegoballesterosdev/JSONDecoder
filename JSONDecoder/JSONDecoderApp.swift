6be2858143754960550e6266304b6398eeb0a8d4
diegoballesterosdev <diegoballesteros.dev@gmail.com>
2023-11-09 20:08:50 +0100

    Initial Commit

---
+++ JSONDecoder/JSONDecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONDecoderApp.swift
+//  JSONDecoder
+//
+//  Created by Diego Ballesteros on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct JSONDecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
